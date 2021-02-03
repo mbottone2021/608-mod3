cec43fdeef12ed88a9168f312a0595912bd1e063
mbottone2021 <77653069+mbottone2021@users.noreply.github.com>
2021-02-02 21:50:59 -0600

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,7 @@
+dataset = [1, 3, 4, 2, 6, 5, 3, 4, 5, 2]
+import statistics
+print('Population Variance: ', statistics.pvariance(dataset))
+print('Standard deviation: ', statistics.pstdev(dataset))
 
+Population Variance:  2.25
+Standard deviation:  1.5
